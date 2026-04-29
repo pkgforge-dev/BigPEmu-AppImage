cf9f0b03f62e4f3c7bcabf6d0e23d50ffc8bd91c
Link4Electronics <link4electronics@gmail.com>
2026-04-29 11:45:37 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+case "$ARCH" in # they use 64 and ARM64 for tar.gz links
+	x86_64)  z_arch=64;;
+	aarch64) z_arch=ARM64;;
+esac
+wget https://www.richwhitehouse.com/jaguar/builds/BigPEmu_Linux${z_arch}_v1221.tar.gz
+tar -xvzf ./*.tar.gz --strip-components=1
+rm -f ./*.tar.gz bigpemu-icon.png make_desktop.sh *.txt 
+mkdir -p ./AppDir/bin
+mv -v Data plugins Scripts Strings bigpemu ./AppDir/bin
