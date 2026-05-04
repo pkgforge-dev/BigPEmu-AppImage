144e1addfebe7d05b4f04bfaac3d423b750ae2c2
Link4Electronics <link4electronics@gmail.com>
2026-05-04 15:49:25 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -24,8 +24,8 @@ case "$ARCH" in # they use AMD64 and ARM64 for the deb links
 	x86_64)  z_arch=64;;
 	aarch64) z_arch=ARM64;;
 esac
-wget https://www.richwhitehouse.com/jaguar/builds/BigPEmu_Linux${z_arch}_v121.tar.gz
-tar -xvzf BigPEmu_Linux${z_arch}_v121.tar.gz --strip-components=1
-rm -f *.tar.gz bigpemu-icon.png make_desktop.sh *.txt 
+wget https://www.richwhitehouse.com/jaguar/builds/BigPEmu_Linux${z_arch}_v122.tar.gz
+tar -xvzf BigPEmu_Linux${z_arch}_v122.tar.gz --strip-components=1
+rm -f ./*.tar.gz bigpemu-icon.png make_desktop.sh *.txt 
 mkdir -p ./AppDir/bin
 mv -v Data plugins Scripts Strings bigpemu ./AppDir/bin
